f0a6ade3d768a705bd0a21f00ce5ea2e5948d840
Sam Puddister <sampuddister@gmail.com>
2022-06-14 16:04:16 -0400

    Built a factory function to create the 2 players. Built in additional functionality to use the player objects to play a game of tic tac toe. Linked the gameboard to specific player/user input.

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+const Player = (name, token) => {
+    let coveredTiles = [];
+    const addTile = (newTileIndex) => {
+        coveredTiles.push(newTileIndex);
+        coveredTiles.sort();
+    }
+    const getTiles = () => coveredTiles;
+    const getName = () => name;
+    const getToken = () => token;
+
+    return {getName, addTile, getTiles, getToken}
+}
+
+const player1 = Player('Player X', 'X');
+const player2 = Player('Player O', 'O');
+
 const gameBoardModule = (() => {
     let gameboard = ['','','','','','','','',''];
+    const winningCombos = [[1,2,3],[4,5,6],[7,8,9],
+                         [1,4,7],[2,5,8],[3,6,9],
+                         [1,5,9],[3,5,7]];
     const startBtn = document.getElementById('startBtn');
     const gameBoardTiles = [...document.getElementsByClassName('tiles')];
     const turnIdentifier = document.getElementsByClassName('card-header-title')[0];
-    let playerToken = 'X';
+    let currentPlayer = player1;
     gameBoardTiles.forEach(tile => {
         tile.addEventListener('click', playValidation)
     });
@@ -11,24 +30,25 @@ const gameBoardModule = (() => {
     function playValidation(e) {
         //Check for valid player move, then call updateGameboard
         let targetTile = e.target.getAttribute('index');
-        updateGameboard(targetTile);
-    }
-    function updateGameboard(tile) {
-        
-        gameBoardTiles[tile].innerText = playerToken;
-        switchTurn();
-    }
-    function switchTurn() {
-        if (playerToken == 'X') {
-            playerToken = 'O';
-        } else {
-            playerToken = 'X';
+        if (gameboard[targetTile] === '') {
+            gameboard[targetTile] = currentPlayer.getToken();
+            currentPlayer.addTile(targetTile);
+            updateGameboard(targetTile);
+            checkForWin();
+            switchTurns();
         }
-        turnIdentifier.innerText = "Player " + playerToken + "'s Turn" 
     }
-    
-    return {
-        bindEvents: bindEvents,
-        gameBoardTiles: gameBoardTiles
+    function updateGameboard(targetTile) {
+        gameBoardTiles[targetTile].innerText = currentPlayer.getToken();
     }
+
+    function checkForWin() {
+        //Compare to winning combinations to determine if victory 
+    }
+
+    function switchTurns() {
+        currentPlayer === player1 ? currentPlayer = player2 : currentPlayer = player1;
+        turnIdentifier.innerText = currentPlayer.getName() + "'s Turn"; 
+    }
+
 })();
