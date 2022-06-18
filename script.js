b8fa9a1a97a23f25ae82466fe21f47170d41914b
Sam Puddister <sampuddister@gmail.com>
2022-06-17 20:39:54 -0400

    Added transform to gameboard.

--- script.js
+++ script.js
@@ -28,7 +28,14 @@ const gameBoardModule = (() => {
     const winningCombos = [[1,2,3],[4,5,6],[7,8,9],
                          [1,4,7],[2,5,8],[3,6,9],
                          [1,5,9],[3,5,7]];
+    const gameContainer = document.getElementsByClassName('game-box')[0];
     const startBtn = document.getElementById('startBtn');
+    startBtn.addEventListener('click', ()=> {
+        gameContainer.classList.add('game-box-transform');
+        gameContainer.classList.remove('game-box');
+        startBtn.classList.remove('game-box-transform');
+        startBtn.classList.add('game-box');
+    });
     const restartBtn = document.getElementById('restartBtn');
     const gameBoardTiles = [...document.getElementsByClassName('tiles')];
     const turnIdentifier = document.getElementsByClassName('card-header-title')[0];
